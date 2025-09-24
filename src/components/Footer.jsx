c5431a6281f318d31f838d91e94f37b874aabc77
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-09-24 15:16:02 +0300

    added footer for my project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,67 @@
+<footer class="flex min-h-30 w-full items-center justify-around border-2 border-t-amber-50 bg-gray-950">
+  <div class="flex min-h-full flex-col justify-center space-y-4">
+    <h1 class="text-white">Biniyam Girma</h1>
+    <div class="flex items-center justify-center space-x-4 text-white">
+      <a href="">portfolio</a>
+      <span class="flex items-center justify-center text-white"
+        ><svg class="size-2" viewBox="0 0 24 24" fill="none">
+          <path d="M8 8.5L11.5 12L8 15.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M14 8.5L17.5 12L14 15.5" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg
+      ></span>
+      <a href="">portfolio</a>
+      <span class="flex items-center justify-center text-white"
+        ><svg class="size-2" viewBox="0 0 24 24" fill="none">
+          <path d="M8 8.5L11.5 12L8 15.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M14 8.5L17.5 12L14 15.5" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg
+      ></span>
+      <a href="">portfolio</a>
+    </div>
+  </div>
+  <div class="flex flex-col space-y-4">
+    <p class="text-white">contact us</p>
+    <ul class="flex space-x-4">
+      <li class="flex size-8 items-center justify-center rounded-full bg-amber-50 text-black">
+        <span class="flex items-center justify-center text-white"
+          ><svg class="size-2" viewBox="0 0 24 24" fill="none">
+            <path d="M8 8.5L11.5 12L8 15.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M14 8.5L17.5 12L14 15.5" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg
+        ></span>
+      </li>
+      <li class="flex size-8 items-center justify-center rounded-full bg-amber-50 text-black">
+        <span class="flex items-center justify-center text-white"
+          ><svg class="size-2" viewBox="0 0 24 24" fill="none">
+            <path d="M8 8.5L11.5 12L8 15.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M14 8.5L17.5 12L14 15.5" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg
+        ></span>
+      </li>
+      <li class="flex size-8 items-center justify-center rounded-full bg-amber-50 text-black">
+        <span class="flex items-center justify-center text-white"
+          ><svg class="size-2" viewBox="0 0 24 24" fill="none">
+            <path d="M8 8.5L11.5 12L8 15.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M14 8.5L17.5 12L14 15.5" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg
+        ></span>
+      </li>
+      <li class="flex size-8 items-center justify-center rounded-full bg-amber-50 text-black">
+        <span class="flex items-center justify-center text-white"
+          ><svg class="size-2" viewBox="0 0 24 24" fill="none">
+            <path d="M8 8.5L11.5 12L8 15.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M14 8.5L17.5 12L14 15.5" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg
+        ></span>
+      </li>
+      <li class="flex size-8 items-center justify-center rounded-full bg-amber-50 text-black">
+        <span class="flex items-center justify-center text-white"
+          ><svg class="size-2" viewBox="0 0 24 24" fill="none">
+            <path d="M8 8.5L11.5 12L8 15.5" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M14 8.5L17.5 12L14 15.5" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#D3DAD9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" /></svg
+        ></span>
+      </li>
+    </ul>
+  </div>
+</footer>
