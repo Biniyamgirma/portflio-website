5cf94c4e9b49fa1a9864af50c27b734d3958ba60
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-09-25 10:39:20 +0300

    added projects section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,30 @@
+<section class="flex h-screen w-full flex-col items-center bg-gray-950">
+  <div class="h-auto w-60/100">
+    <button class="my-4 rounded-full bg-[#3939395C] px-4 py-2 text-[#FF9853]">Milestones</button>
+    <h4 class="text-4xl font-semibold wrap-break-word text-[#FFFFFF] md:w-60/100">Token Sale Phases & Milestones</h4>
+    <div id="project" class="my-8 flex h-auto w-50/100 flex-col">
+      <button class="max-w-[120px] rounded-full bg-[#FF9853] px-4 py-2 text-white">Project Link</button>
+      <p class="mt-4 min-w-full wrap-anywhere text-white">Project 1 - Ethiopian Missing Person Finding System</p>
+      <div class="flex w-full flex-col">
+        <div class="mt-4 flex w-full justify-between">
+          <h6 class="text-gray-700">Progress</h6>
+          <h6 class="text-white">100%</h6>
+        </div>
+        <div class="mt-2 h-2 w-full rounded-full bg-gray-200">
+          <div class="bg-white-600 h-full rounded-full" style="width: 100%;"></div>
+        </div>
+      </div>
+      <div class="mt-4 w-full">
+        <h4 class="text-white">Tech stack Used:</h4>
+        <div class="mt-2 flex items-center space-x-2">
+          <span class="size-2 rounded-full bg-amber-50"></span>
+          <p class="font-semibold text-gray-600 uppercase">react</p>
+        </div>
+        <div class="mt-1 flex items-center space-x-2">
+          <span class="size-2 rounded-full bg-amber-50"></span>
+          <p class="font-semibold text-gray-600 uppercase">Tailwindcss</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
