7cd093df60e580664ce451932d9862dd2dfaa3af
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-09-26 12:29:53 +0300

    removed one skill

--- src/components/TechSkill.jsx
+++ src/components/TechSkill.jsx
@@ -8,11 +8,11 @@ function TechSkill() {
     <div className="h-full w-full">
       <div className="flex w-full items-end justify-between my-4">
         <div className="flex flex-col space-y-2 ">
-          <h2 className="text-[#DFDFDF]">Category</h2>
-          <p className="text-[#717171]">Allocation Breakdown category</p>
+          <h2 className="text-[#DFDFDF]">Skills</h2>
+          <p className="text-[#717171]">Frameworks And Programming languages</p>
         </div>
         <div>
-          <h2 className="text-[#FF9853]">Allocation (%)</h2>
+          <h2 className="text-[#FF9853]">Skill in  (%)</h2>
         </div>
       </div>
     </div>
@@ -63,30 +63,8 @@ function TechSkill() {
           <div className="h-2.5 rounded-full bg-blue-600" style={{ width: '45%'}}></div>
         </div>
       </div>
-    </div><div className="flex w-full flex-col">
-      <div className="mt-4 flex w-full justify-between">
-        <div className="ml-2 flex" id="skill wraper">
-          <span className="mr-2 flex items-center justify-center">
-            <svg className="size-4" viewBox="0 0 24 24" fill="none">
-              <path d="M8 8.5L11.5 12L8 15.5" stroke="#717171" stroke-width="1.5" stroke-linecap="round" strokeLinejoin="round" />
-              <path d="M14 8.5L17.5 12L14 15.5" stroke="#717171" stroke-width="1.5" stroke-linecap="round" strokeLinejoin="round" />
-              <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#717171" stroke-width="1.5" stroke-linecap="round" strokeLinejoin="round" />
-            </svg>
-          </span>
-          <h3 className="text-[#717171]">Public Sale</h3>
-        </div>
-        <div className="flex items-center justify-center space-x-3">
-          <h3 className="text-[#FFFFFF]">30%</h3>
-          <span className="flex size-2 items-center justify-center rounded-full bg-[#717171]"> </span>
-          <p className="text-[#717171]">Allocation</p>
-        </div>
-      </div>
-      <div classNameName="mt-2">
-        <div classNameName="h-2.5 w-full rounded-full bg-gray-200">
-          <div classNameName="h-2.5 rounded-full bg-[#FF9853]" style={{ width: '45%'}}></div>
-        </div>
-      </div>
     </div>
+    
   </div>
 </section>
   )
