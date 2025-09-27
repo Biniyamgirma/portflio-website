a31699e5161bb94fd5babbe5f9373140430c9826
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-09-28 00:06:09 +0300

    added a cool marquee animation text effect on skills component

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,74 +1,145 @@
 import React,{useRef} from 'react'
-import {useGSAP} from 'react-gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap  from 'gsap';
+import { useGSAP } from '@gsap/react';
 
-
+function horizontalLoop(items, config) {
+	items = gsap.utils.toArray(items);
+	config = config || {};
+	let tl = gsap.timeline({repeat: config.repeat, paused: config.paused, defaults: {ease: "none"}, onReverseComplete: () => tl.totalTime(tl.rawTime() + tl.duration() * 100)}),
+		length = items.length,
+		startX = items[0].offsetLeft,
+		times = [],
+		widths = [],
+		xPercents = [],
+		curIndex = 0,
+		pixelsPerSecond = (config.speed || 1) * 100,
+		snap = config.snap === false ? v => v : gsap.utils.snap(config.snap || 1), // some browsers shift by a pixel to accommodate flex layouts, so for example if width is 20% the first element's width might be 242px, and the next 243px, alternating back and forth. So we snap to 5 percentage points to make things look more natural
+		totalWidth, curX, distanceToStart, distanceToLoop, item, i;
+	gsap.set(items, { // convert "x" to "xPercent" to make things responsive, and populate the widths/xPercents Arrays to make lookups faster.
+		xPercent: (i, el) => {
+			let w = widths[i] = parseFloat(gsap.getProperty(el, "width", "px"));
+			xPercents[i] = snap(parseFloat(gsap.getProperty(el, "x", "px")) / w * 100 + gsap.getProperty(el, "xPercent"));
+			return xPercents[i];
+		}
+	});
+	gsap.set(items, {x: 0});
+	totalWidth = items[length-1].offsetLeft + xPercents[length-1] / 100 * widths[length-1] - startX + items[length-1].offsetWidth * gsap.getProperty(items[length-1], "scaleX") + (parseFloat(config.paddingRight) || 0);
+	for (i = 0; i < length; i++) {
+		item = items[i];
+		curX = xPercents[i] / 100 * widths[i];
+		distanceToStart = item.offsetLeft + curX - startX;
+		distanceToLoop = distanceToStart + widths[i] * gsap.getProperty(item, "scaleX");
+		tl.to(item, {xPercent: snap((curX - distanceToLoop) / widths[i] * 100), duration: distanceToLoop / pixelsPerSecond}, 0)
+		  .fromTo(item, {xPercent: snap((curX - distanceToLoop + totalWidth) / widths[i] * 100)}, {xPercent: xPercents[i], duration: (curX - distanceToLoop + totalWidth - curX) / pixelsPerSecond, immediateRender: false}, distanceToLoop / pixelsPerSecond)
+		  .add("label" + i, distanceToStart / pixelsPerSecond);
+		times[i] = distanceToStart / pixelsPerSecond;
+	}
+	function toIndex(index, vars) {
+		vars = vars || {};
+		(Math.abs(index - curIndex) > length / 2) && (index += index > curIndex ? -length : length); // always go in the shortest direction
+		let newIndex = gsap.utils.wrap(0, length, index),
+			time = times[newIndex];
+		if (time > tl.time() !== index > curIndex) { // if we're wrapping the timeline's playhead, make the proper adjustments
+			vars.modifiers = {time: gsap.utils.wrap(0, tl.duration())};
+			time += tl.duration() * (index > curIndex ? 1 : -1);
+		}
+		curIndex = newIndex;
+		vars.overwrite = true;
+		return tl.tweenTo(time, vars);
+	}
+	tl.next = vars => toIndex(curIndex+1, vars);
+	tl.previous = vars => toIndex(curIndex-1, vars);
+	tl.current = () => curIndex;
+	tl.toIndex = (index, vars) => toIndex(index, vars);
+	tl.times = times;
+  if (config.reversed) {
+    tl.vars.onReverseComplete();
+    tl.reverse();
+  }
+	return tl;
+}
 function Skill() {
   const ref = useRef(null);
   useGSAP(()=>{
+    const array = gsap.utils.toArray(".skill-item");
+    array.forEach((line, i)=>{
+        const links = line.querySelectorAll("span"),
+         tl = horizontalLoop(links, {
+          repeat: -1, 
+          speed: 1 + i * 0.5,
+          reversed: i ? true : false,
+          paddingRight: parseFloat(gsap.getProperty(links[0], "marginRight", "px")) // otherwise first element would be right up against the last when it loops. In this layout, the spacing is done with marginRight.
+        });
+        links.forEach(link => {
+    link.addEventListener("mouseenter", () => gsap.to(tl, {timeScale: 0, overwrite: true}));
+    link.addEventListener("mouseleave", () => gsap.to(tl, {timeScale: i ? -1 : 1, overwrite: true}));
+    });
+    });
     
-  },{})
+
+
+  },{scope:ref})
   return (
-    <section ref={ref} className="bg-gray-950 w-full h-100 flex flex-col justify-center items-center overflow-x-clip">
+  <section ref={ref} className="bg-gray-950 w-full h-100 flex flex-col justify-center items-center overflow-x-clip">
   <h1 className="text-white font-bold uppercase text-4xl mb-8">Soft Skills</h1>
   <div className="w-1000 h-32 bg-[#121212] -rotate-4  py-8">
-    <div className="flex items-center justify-center h-full  space-x-4">
+    <div className="flex items-center justify-center h-full  space-x-4 will-change-transform skill-item">
       <span className="text-[#FFFFFF] font-bold text-2xl ml-2">lL</span>
       <span className="text-[#FFFFFF]">
        <svg className="size-8" viewBox="0 0 24 24" fill="none" >
         <path d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z" fill="#78B9B5"/>
         </svg>
       </span>
-      <p className="font-semibold text-[#6B6B6B] new-font-ibm">Verbal Communication</p>
+      <span><p className="font-semibold text-[#6B6B6B] new-font-ibm">Verbal Communication</p></span>
       <span className="text-[#FFFFFF] font-bold text-2xl mx-2">lL</span>
       <span className="text-[#FFFFFF]">
        <svg className="size-8" viewBox="0 0 24 24" fill="none" >
         <path d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z" fill="#B13BFF"/>
         </svg>
       </span>
-      <p className="font-semibold text-[#6B6B6B] new-font-ibm">Public Speaking</p>
+      <span><p className="font-semibold text-[#6B6B6B] new-font-ibm">Public Speaking</p></span>
       <span className="text-[#FFFFFF] font-bold text-2xl ml-2">lL</span>
       <span className="text-[#FFFFFF]">
        <svg className="size-8" viewBox="0 0 24 24" fill="none" >
         <path d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z" fill="#FF9853"/>
         </svg>
       </span>
-      <p className="font-semibold text-[#6B6B6B] new-font-ibm">Teamwork</p>
+      <span><p className="font-semibold text-[#6B6B6B] new-font-ibm">Teamwork</p></span>
       <span className="text-[#FFFFFF] font-bold text-2xl ml-2">lL</span>
       <span className="text-[#FFFFFF]">
        <svg className="size-8" viewBox="0 0 24 24" fill="none" >
         <path d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z" fill="#FFCC00"/>
         </svg>
       </span>
-      <p className="font-semibold text-[#6B6B6B] new-font-ibm">Collaboration</p>
+      <span><p className="font-semibold text-[#6B6B6B] new-font-ibm text-nowrap">Collaboration</p></span>
       <span className="text-[#FFFFFF] font-bold text-2xl ml-2">lL</span>
       <span className="text-[#FFFFFF]">
        <svg className="size-8" viewBox="0 0 24 24" fill="none" >
         <path d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z" fill="#EF88AD"/>
         </svg>
       </span>
-      <p className="font-semibold text-[#6B6B6B] new-font-ibm">Critical Thinking</p>
+      <span><p className="font-semibold text-[#6B6B6B] new-font-ibm">Critical Thinking</p></span>
       <span className="text-[#FFFFFF] font-bold text-2xl ml-2">lL</span>
       <span className="text-[#FFFFFF]">
        <svg className="size-8" viewBox="0 0 24 24" fill="none" >
         <path d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z" fill="#1F7D53"/>
         </svg>
       </span>
-      <p className="font-semibold text-[#6B6B6B] new-font-ibm">Creativity</p>
+      <span><p className="font-semibold text-[#6B6B6B] new-font-ibm">Creativity</p></span>
       <span className="text-[#FFFFFF] font-bold text-2xl ml-2">lL</span>
       <span className="text-[#FFFFFF]">
        <svg className="size-8" viewBox="0 0 24 24" fill="none" >
         <path d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z" fill="#DDE6ED"/>
         </svg>
       </span>
-      <p className="font-semibold text-[#6B6B6B] new-font-ibm">Time Management</p>
+      <span><p className="font-semibold text-[#6B6B6B] new-font-ibm">Time Management</p></span>
       <span className="text-[#FFFFFF] font-bold text-2xl ml-2">lL</span>
       <span className="text-[#FFFFFF]">
        <svg className="size-8" viewBox="0 0 24 24" fill="none" >
         <path d="M12 9.5C13.3807 9.5 14.5 10.6193 14.5 12C14.5 13.3807 13.3807 14.5 12 14.5C10.6193 14.5 9.5 13.3807 9.5 12C9.5 10.6193 10.6193 9.5 12 9.5Z" fill="#03C988"/>
         </svg>
       </span>
-      <p className="font-semibold text-[#6B6B6B] new-font-ibm">Work Ethic</p>
+      <span><p className="font-semibold text-[#6B6B6B] new-font-ibm">Work Ethic</p></span>
     </div>
   </div>
 </section>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+//step 1 I had regstered the plugin here
+gsap.registerPlugin(ScrollTrigger);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
